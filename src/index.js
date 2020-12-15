a19d12cb66c75f3d46711b4bb3153a4e3bf160bf
ghaiyee <ghaiyee@gmail.com>
2020-12-15 20:43:31 +0800

    use gtm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TagManager from "react-gtm-module";
+
 import Routes from "./routes";
+
+const GTM_ID = "GTM-MSS48J4";
+
+TagManager.initialize({
+  gtmId: GTM_ID,
+});
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
