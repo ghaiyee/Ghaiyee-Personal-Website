2049a4c241b0f24cd57dc7181e1ece770eb36c17
ghaiyee <ghaiyee@gmail.com>
2021-01-06 23:52:01 +0800

    added project2 process image

--- src/components/Project2/projectContent.js
+++ src/components/Project2/projectContent.js
@@ -51,7 +51,8 @@ export default function ProjectContent() {
   const classes = useStyles();
 
   const content = {
-   challenges: "Learning & Developement (L&D) professionals often got involved in fire-fighting mode such as running ad-hoc training, promoting training to targeted audiences, registering attendees, multiple reminders to different attendees at different dates and time and churning out individual training report when required. ",
+    challenges:
+      "Learning & Developement (L&D) professionals often got involved in fire-fighting mode such as running ad-hoc training, promoting training to targeted audiences, registering attendees, multiple reminders to different attendees at different dates and time and churning out individual training report when required. ",
     process: "Work in Progress",
     research: " ",
   };
@@ -67,9 +68,11 @@ export default function ProjectContent() {
           </Grid>
           <Grid item md={12} xs={12}>
             <Typography className={classes.title}>Process</Typography>
-            <Typography className={classes.content}>
-              {content.process}
-            </Typography>
+            <img
+              src="excellenceHubProcess.png"
+              width="100%"
+              className={classes.image}
+            />
           </Grid>
         </Grid>
       </Container>
